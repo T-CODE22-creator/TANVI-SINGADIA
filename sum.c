7fd534e60dbec0112dcc790f82f9f7d6dcb07f93
T-CODE22-creator <tanvi.37467@stu.upes.ac.in>
2026-08-10 21:38:36 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+
+    printf("Enter first number:");
+    scanf("%d", &a);
+    printf("Enter second number:");
+    scanf("%d", &b);
+    
+    printf("Sum = %d", a+b);
+    return 0;
+}
