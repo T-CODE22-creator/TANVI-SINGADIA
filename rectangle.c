64e5b249caad6f202d9429904dc32dc92db56c38
T-CODE22-creator <tanvi.37467@stu.upes.ac.in>
2026-08-11 23:34:31 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,12 @@
+/*write a program to calculate the area and perimeter of a rectangle given its length and breadth.*/
+#include <stdio.h>
+int main()
+{int l, b ;
+    printf("enter value of l:\n");
+    scanf("%d", &l);
+    printf("enter value of b:\n");
+    scanf("%d", &b);
+printf("area:%d\n", l*b);
+printf("perimeter:%d\n", 2*(l+b));
+    return 0;
+}
