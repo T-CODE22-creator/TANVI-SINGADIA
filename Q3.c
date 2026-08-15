2a924744e13cda071af93744fe3d85ea6bdd5ce9
T-CODE22-creator <tanvi.37467@stu.upes.ac.in>
2026-08-15 13:28:37 +0530

    Update and rename rectangle.c to Q3.c

--- Q3.c
+++ Q3.c
@@ -1,4 +1,4 @@
-/*write a program to calculate the area and perimeter of a rectangle given its length and breadth.*/
+/*Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.*/
 #include <stdio.h>
 int main()
 {int l, b ;
