160772d0aeeda7873e1746f7ff85d5e75b0a975b
T-CODE22-creator <tanvi.37467@stu.upes.ac.in>
2026-08-15 13:35:31 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,8 +1,7 @@
 /*Q2. write a program to input two numbers and display their sum, difference, product, and quotient.*/
 #include <stdio.h>
 int main()
-{
-    int a,b;
+{int a,b;
     printf("enter value of a:\n");
     scanf("%d", &a);
     printf("enter value of b:\n");
@@ -11,6 +10,5 @@ int main()
     printf("difference:%d\n" , a-b);
     printf("product:%d\n" , a*b);
     printf("quotient:%d\n" , a/b);
-    
-    return 0;
+      return 0;
 }
